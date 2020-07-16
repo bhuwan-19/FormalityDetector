f8ca4395df18fb9560e17d25480c050c51b8ae91
bhuwan-19 <bhuwan_19@outlook.com>
2020-07-16 23:10:23 +0300

    model

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 
 MODEL_DIR = os.path.join(CUR_DIR, 'utils', 'model')
-FORMALITY_MODEL = os.path.join(MODEL_DIR, 'answers.light.clf')
+FORMALITY_MODEL = os.path.join(MODEL_DIR, 'formality.clf')
 WORD2VEC_MODEL = os.path.join(MODEL_DIR, 'GoogleNews-vectors-negative300.bin')
 
 COEFFICIENT_A = 2.0
